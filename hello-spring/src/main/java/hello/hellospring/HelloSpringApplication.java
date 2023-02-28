01811dd0c4769d3ba7d1d163d4f85c4116cec353
yjs <wptmd97@naver.com>
2023-02-28 14:46:35 +0900

    0228.2

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,6 +2,7 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloSpringApplication {
@@ -9,5 +10,4 @@ public class HelloSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
-
 }
